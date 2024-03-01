85c9c714bc03d6e1735537cff837cb1dae7866b4
Kislay Raj <kislayraj751@gmail.com>
2024-03-01 19:42:38 +0530

    cursor pointer

--- src/components/maincontent.js
+++ src/components/maincontent.js
@@ -9,10 +9,10 @@ const MainContent= () => {
      <div className='pt-2 '>Interests</div>
      <div className='text-xs mt-1'>As a web developer, I'm passionate about coding, UX design, problem-solving, and staying updated with tech trends.</div>
      <div className="follow mt-3 m-1 flex justify-between text-3xl text-white ">
-      <div className="git"><i class="fa-brands fa-square-github"></i></div>
-      <div className='twitter'><i class="fa-brands fa-square-x-twitter"></i></div>
-      <div className='fb'><i class="fa-brands fa-facebook"></i></div>
-      <div className='insta'><i class="fa-brands fa-square-instagram"></i></div>
+      <div className="git cursor-pointer"><i class="fa-brands fa-square-github"></i></div>
+      <div className='twitter cursor-pointer'><i class="fa-brands fa-square-x-twitter"></i></div>
+      <div className='fb cursor-pointer'><i class="fa-brands fa-facebook"></i></div>
+      <div className='insta cursor-pointer'><i class="fa-brands fa-square-instagram"></i></div>
      </div>
       </div>
     
